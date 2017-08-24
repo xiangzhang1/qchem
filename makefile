2637e0d149b69a53e9dfe763ef0105a885b931c1
Xiang Zhang <zhang-xiang@hotmail.com>
2017-08-24 13:26:16 -0400

    removed src.py eternally!

--- makefile
+++ makefile
@@ -1,9 +1,5 @@
 .PHONY : compile, clean, run, test
-compile : *.src.py
-	for f in *.src.py ; do sed "s/'''//g" "$$f" > "$${f%.src.py}.py"; done
-clean :
-	for f in *.src.py ; do rm -rf "$${f%.src.py}.py"; done; rm -rf *.pyc
 run : compile
 	flask run
-test : 
+test :
 	python test.py
--- shared.src.py
+++ shared.src.py
@@ -269,14 +269,13 @@ class CustomError(Exception):
 
 # MySFTPClient: A wrapper for paramiko
 
-import paramiko
-import os
-
 class MySFTPClient(paramiko.SFTPClient):
+    '''extension of paramiko SFTPClient'''
+
     def put_dir(self, source, target):
-        # Uploads the contents of the source directory to the target path. The
-        #    target directory needs to exists. All subdirectories in source are
-        #    created under target.
+        ''' Uploads the contents of the source directory to the target path. The
+            target directory needs to exists. All subdirectories in source are
+            created under target.'''
         for item in os.listdir(source):
             if os.path.isfile(os.path.join(source, item)):
                 self.put(os.path.join(source, item), '%s/%s' % (target, item))
@@ -285,7 +284,7 @@ class MySFTPClient(paramiko.SFTPClient):
                 self.put_dir(os.path.join(source, item), '%s/%s' % (target, item))
 
     def mkdir(self, path, mode=511, ignore_existing=False):
-        # Augments mkdir by adding an option to not fail if the folder exists  '''
+        '''Augments mkdir by adding an option to not fail if the folder exists'''
         try:
             super(MySFTPClient, self).mkdir(path, mode)
         except IOError:
