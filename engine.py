44d66d044f44056103121c7f1958f4514f6b19c0
Xiang Zhang <zhang-xiang@hotmail.com>
2017-09-08 11:24:56 -0400

    automated push

--- engine.py
+++ engine.py
@@ -1440,19 +1440,19 @@ class Compare(object):
                                                                                                                                            e[idx_max][0], eoc.ccoor[int(e[idx_max][0])], e[idx_max][1], eoc.ccoor[int(e[idx_max][1])], b[idx_max,2],
                                                                                                                                            np.abs(b-e)[:,2].max())
             self.log += '-' * 130 + '\n'
-            
+
             # bijective-representation difference (congruent testing), allowing physical phenomena relocation
             b = b[ b[:,2].argsort() ]
             e = e[ e[:,2].argsort() ]
             self.log += u'<arbitrary-order bijective-representation difference> (allowing physical phenomena relocation) between self and backdrop is: \n'
             idx_min = np.abs(b-e)[:,2].argmin()
             self.log += u'    min difference: backdrop_pdist [%2d(%s)-%2d(%s)=%.3f] - electron_pdist [%2d(%s)-%2d(%s)=%.3f] = %f \u212B. \n' %(b[idx_min][0], boc.ccoor[int(b[idx_min][0])], b[idx_min][1], boc.ccoor[int(b[idx_min][1])], b[idx_min,2],
-                                                                                                                                                       e[idx_min][0], eoc.ccoor[int(e[idx_min][0])], e[idx_min][1], eoc.ccoor[int(e[idx_min][1])], b[idx_min,2],
+                                                                                                                                                       e[idx_min][0], eoc.ccoor[int(e[idx_min][0])], e[idx_min][1], eoc.ccoor[int(e[idx_min][1])], e[idx_min,2],
                                                                                                                                                        np.abs(b-e)[:,2].min())
             self.log += u'    avg difference: %s \u212B. \n' %(abs(b-e)[:,2].mean())
             idx_max = abs(b-e)[:,2].argmax()
             self.log += u'    max difference: backdrop_pdist [%2d(%s)-%2d(%s)=%.3f] - electron_pdist [%2d(%s)-%2d(%s)=%.3f] = %f \u212B. \n' %(b[idx_max][0], boc.ccoor[int(b[idx_max][0])], b[idx_max][1], boc.ccoor[int(b[idx_max][1])], b[idx_max,2],
-                                                                                                                                           e[idx_max][0], eoc.ccoor[int(e[idx_max][0])], e[idx_max][1], eoc.ccoor[int(e[idx_max][1])], b[idx_max,2],
+                                                                                                                                           e[idx_max][0], eoc.ccoor[int(e[idx_max][0])], e[idx_max][1], eoc.ccoor[int(e[idx_max][1])], e[idx_max,2],
                                                                                                                                            np.abs(b-e)[:,2].max())
 
 
