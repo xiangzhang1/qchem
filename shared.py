0e9e8ca7d329c376b99377a32836ac36014978d3
Xiang Zhang <zhang-xiang@hotmail.com>
2017-09-25 01:27:30 -0400

    automated push

--- shared.py
+++ shared.py
@@ -37,8 +37,8 @@ import traceback, sys, code
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 HOME_DIR = os.path.expanduser(os.path.expanduser("~"))
 
-# DEBUG = 1       # -1: print nothing. 0: concise (recommended). 1: debug (slightly untidy). 2: disaster (code error)
-DEBUG = 0
+DEBUG = 1       # -1: print nothing. 0: concise (recommended). 1: debug (slightly untidy). 2: disaster (code error)
+# DEBUG = 0
 
 # Nodes
 # ===========================================================================
