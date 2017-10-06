b1cd5af2d0211a2839a1475e013ace925b3298d5
Xiang Zhang <zhang-xiang@hotmail.com>
2017-10-06 13:23:01 -0400

    automated push

--- engine.py
+++ engine.py
@@ -1534,7 +1534,6 @@ class Movie(object):
         # each step
         istep = 0
         for ionicstep in root.findall('calculation'):
-            print 'loopstart,',
             structure = ionicstep.find('structure')
             # base
             base = []
@@ -1551,19 +1550,12 @@ class Movie(object):
             # ccoor
             ccoor = np.dot(fcoor, base)
             # draw movie frame
-            print 'loop1,',
             fig = plt.figure()
-            print 'loop2,',
             ax = fig.add_subplot(111, projection='3d')
-            print 'loop3,',
             xs, ys, zs = list(ccoor[:,0]), list(ccoor[:,1]), list(ccoor[:,2])
-            print 'loop4,',
-            ax.scatter([1,2,3], [1,2,3], [1,2,3], s=15)
-            print 'loop5,',
+            ax.scatter(xs, ys, zs, s=15)
             ax.set_axis_off()
-            print 'loop6,',
             plt.savefig(electron.path+'/%s.png' %(istep) )
-            print 'loopend,'
         # movie
         os.chdir(electron.path)
         os.system('avconv -f image2 -r 1 -i %d.png -vcodec mpeg4 -y movie.mp4')
