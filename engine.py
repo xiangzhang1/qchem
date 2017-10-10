4f637158462194788d76e428f4c4f0a7da331bee
Xiang Zhang <zhang-xiang@hotmail.com>
2017-10-09 20:31:16 -0400

    automated push

--- engine.py
+++ engine.py
@@ -336,8 +336,6 @@ class Gen(object):  # Stores the logical structure of keywords and modules. A un
             raise shared.CustomError(self.__class__.__name__ + 'error: makeparam output illegal. Check POSCAR4 format and memory leak in script dir.')
         # parse and raise error
         memory_required = ( (self.memory['projector_real'] + self.memory['projector_reciprocal'])*int(self.getkw('npar')) + self.memory['wavefunction']*float(self.getkw('kpar')) )/1024.0/1024/1024 + int(self.getkw('nnode'))*0.7
-        with open("/home/xzhang1/m_cpu_config.log", "a") as of_:
-            of_.write('%s %s %s %s %s %s\n' %(self.memory['arraygrid'], self.memory['projector_real'], self.memory['projector_reciprocal'], self.getkw('npar'), self.getkw('kpar'), int(self.getkw('nnode')) * int(self.getkw('ncore_node')) ) )
         memory_required *= multiplier
         memory_available = int(self.getkw('nnode')) * int(self.getkw('mem_node'))
         if memory_required > memory_available:
