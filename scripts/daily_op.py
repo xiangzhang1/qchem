8d6e70839b221f0bb0c2d1f58e8ef578ad4d0b2b
Xiang Zhang <zhang-xiang@hotmail.com>
2017-10-10 22:01:22 -0400

    automated push

--- scripts/daily_op.py
+++ scripts/daily_op.py
@@ -7,6 +7,6 @@ p = engine.Map().lookup('master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S
 p.map.add_edge('10alt -0_02 opt', 'compare cell')
 n = p.map.lookup('compare cell')
 
+n.property = 'engine=md, movie, elev=10, angle=1'
 n.reset()
-n.property = 'engine=md, movie, elev=30, angle=1'
 n.compute()
