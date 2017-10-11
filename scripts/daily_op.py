24248aac46489b8b06ab1558bda18c16b76ec914
Xiang Zhang <zhang-xiang@hotmail.com>
2017-10-11 03:38:36 -0400

    automated push

--- scripts/daily_op.py
+++ scripts/daily_op.py
@@ -3,9 +3,9 @@ import os
 
 qchem.Load()
 
-p = engine.Map().lookup('master.PbS QD.bare qd testing.crunchit')
-p.map.add_edge('7 opt', 'md')
-n = p.map.lookup('md')
+p = engine.Map().lookup('master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38')
+p.map.add_edge('20alt -0_02 opt contd', 'compare cell')
+n = p.map.lookup('compare cell')
 
 n.property = 'engine=md, movie, elev=10, angle=1'
 n.reset()
--- scripts/ml.py
+++ scripts/ml.py
@@ -13,12 +13,14 @@ with open("m_cpu_config.log", "r") as if_:
 data = np.float_(data)
 
 
-data = preprocessing.normalize(data, norm='l2', axis=0)
-scalar = preprocessing.StandardScaler().fit(data)
-data=scalar.transform(data)
+# data = preprocessing.normalize(data, norm='l2', axis=0)
+data[:, (0,1,3,5)] /= 1000000000
+data[:, (-3, -2)] /= 1000
+
+# scalar = preprocessing.StandardScaler().fit(data[:, :-3])
+# data[:, :-3] = scalar.transform(data[:, :-3])
 
 svr = SVR(kernel='poly',degree=3)
-
 
 print '-' * 100
 
@@ -26,18 +28,18 @@ print '-' * 100
 svr.fit(data[:300,:-3],data[:300,-2])
 
 #testing
-predictions=svr.predict(data[300:,:-3])
+predictions=svr.predict(data[300:350,:-3])
 
-for o,p,d in zip(data[300:,-2], predictions, data[300:,-3]):
+for o,p,d in zip(data[300:350,-2], predictions, data[300:350,-3]):
    print d, o, p
 
-print '-' * 100
-
-#train
-svr.fit(data[:300,:-3],data[:300,-1])
-
-#testing
-predictions=svr.predict(data[300:,:-3])
-
-for p,d in zip(predictions, data[300:,-1]):
-   print d, p
+# print '-' * 100
+#
+# #train
+# svr.fit(data[:300,:-3],data[:300,-1])
+#
+# #testing
+# predictions=svr.predict(data[300:350,:-3])
+#
+# for p,d in zip(predictions, data[300:350,-1]):
+#    print d, p
