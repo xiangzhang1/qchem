1e2b4cb0de255077f841eaebe5c5a8e58cd2a261
Xiang Zhang <zhang-xiang@hotmail.com>
2017-10-20 17:20:50 -0400

    automated push

---
+++ scripts/ml/a.py
@@ -0,0 +1,18 @@
+# simply and shitty (aka don't lose the simplest intuition, eg de-pair the shit)
+# feed multiple copies
+# 交替anneal
+# always near. don't do ultimatum.
+# 2 types on.
+# train force.
+
+1000 samples - 70 nodes of activation and LC+bias
+
+# pair first. then the rest to high-dim.
+# pair result is rotationally invariant (but not necessarily axial!)
+
+# hand symmetry:
+#
+
+# globally:
+# x is always x, or so say -y, -z, -y-z, -x
+# force alway 0. newton's only thing.
--- scripts/pre_optimization_regression.py
+++ scripts/pre_optimization_regression.py
@@ -72,3 +72,51 @@ def get_error(x):
 print minimize(get_error, method='Powell', x0=[  6.155e-01,   1.783e+00,  -6.703e+01,   3.578e+01,   9.838e+02, -7.022e+02])
 #[0.03, 0.3, 0.1, 0.2, 0.3, 0.5]
 #[  6.155e-01,   1.783e+00,  -6.703e+01,   3.578e+01,   9.838e+02, -7.022e+02]
+
+
+
+
+
+
+
+qchem.Load()
+
+def pre_optimize_regular(cell, a, b, c, d, e, f):     # start ccoor, parameters
+    import numpy as np
+    ecell = engine.Cell(str(cell))
+    sc = ecell.ccoor
+    ec = np.copy(sc)
+    for i, j in np.ndindex(len(sc), len(sc)):
+        if i==j: continue
+        x = sc[j] - sc[i]
+        r = np.linalg.norm(x)
+        ec[i] += x / r * a * np.exp(-b * r) * (c * r + d + e / r + f / r**2 )
+    ecell.ccoor = ec
+    return ecell
+
+a, b, c, d, e, f = [  7.143e-01,   1.567e+00,  -7.283e+01,   3.575e+01,   9.846e+02,  -7.600e+02]
+
+cur_list = [
+            ['master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - regular grid | end - -0_02 | omg i forgot isym0','master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - regular grid | end - -0_02 | omg i forgot isym0'],
+            ['master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.10alt -0_02 opt','master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.10alt -0_02 opt'],
+            ['master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.20alt -0_02 opt','master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.20alt -0_02 opt contd'],
+            ['master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.15alt -0_02 opt','master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.15alt -0_02 opt'],
+            ['master.PbS QD.bare qd testing.Q0 Reproduce.Pb108S108.para opt','master.PbS QD.bare qd testing.Q0 Reproduce.Pb108S108.g opt'],
+            ['master.PbS QD.bare qd testing.Q0 Reproduce.Pb68S68.opt','master.PbS QD.bare qd testing.Q0 Reproduce.Pb68S68.g opt'],
+            ['master.PbS QD.bare qd testing.Q0 Reproduce.Pb63S62.opt','master.PbS QD.bare qd testing.Q0 Reproduce.Pb63S62.g opt'],
+            # ['master.PbS QD.bare qd testing.crunchit.3 opt','master.PbS QD.bare qd testing.crunchit.3 opt'],
+            # ['master.PbS QD.bare qd testing.crunchit.4 opt','master.PbS QD.bare qd testing.crunchit.4 opt'],
+            # ['master.PbS QD.bare qd testing.crunchit.5 opt','master.PbS QD.bare qd testing.crunchit.5 opt'],
+            ['master.PbS QD.bare qd testing.crunchit.6 opt','master.PbS QD.bare qd testing.crunchit.6 opt'],
+            # ['master.PbS QD.bare qd testing.crunchit.7 opt','master.PbS QD.bare qd testing.crunchit.7 opt']
+           ]
+
+for p in cur_list:
+
+   scell = engine.Map().lookup(p[0]).cell
+   ecell = engine.Map().lookup(p[1]).vasp.optimized_cell
+   scell.recompute()
+   ecell.recompute()
+   before_optimization_error = engine.compare_cell_bijective(scell, ecell, suppress_output=True)
+   after_optimization_error = engine.compare_cell_bijective(pre_optimize_regular(scell, a, b, c, d, e, f), ecell, suppress_output=True)
+   print before_optimization_error, after_optimization_error
