6e6baecebe532c902fd4ce2058f65cabf3257977
Xiang Zhang <zhang-xiang@hotmail.com>
2017-10-25 14:40:15 -0400

    automated push

--- engine.py
+++ engine.py
@@ -518,8 +518,8 @@ class Ml_vasp_memory(object):
         X_test[3] /= 10**9
         X_test[5] /= 1000 # in 1000A^3
         # predict
-        Y_test_pred = self.model.predict(np.float_([X_test]).T)
         IPython.embed()
+        Y_test_pred = self.model.predict(np.float_([X_test]).T)
         # reverse scale
         return Y_test_pred * 10**9
 
