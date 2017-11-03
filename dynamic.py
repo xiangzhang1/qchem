9ea494ee5ebe770e5365b023480bdb7d9c752a1f
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-03 17:25:08 -0400

    automated push

--- dynamic.py
+++ dynamic.py
@@ -163,10 +163,7 @@ class MlVaspMemory(object):
             loss = tf.nn.l2_loss(y - y_batch)
             optimizer = tf.train.GradientDescentOptimizer(learning_rate)
             training_op = optimizer.minimize(loss)  # remember to wipe your ass!
-        # with tf.variable_scope('summarize'):
-        #     summary_op = tf.summary.scalar('loss', loss)
         saver = tf.train.Saver()
-        # summary_writer = tf.summary.FileWriter(self.path+'_summary', tf.get_default_graph())
 
         # ANN: execute
         with tf.Session() as sess:
@@ -175,7 +172,8 @@ class MlVaspMemory(object):
                 for _ in range(data.shape[0] // batch_size):
                     sess.run([update_ops, training_op])
                 if epoch % 50 == 0:
-                    print sess.run([y, y_batch])
+                    print sess.run([y, y_batch, loss])
+                    print '-' * 50
             saver.save(sess, self.path)
 
 
