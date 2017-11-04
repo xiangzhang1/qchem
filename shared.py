3724baf3e475693512c4a7b931a14ad93c56e2e7
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-03 23:15:09 -0400

    automated push

--- shared.py
+++ shared.py
@@ -38,7 +38,7 @@ from StringIO import StringIO
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
 HOME_DIR = os.path.expanduser(os.path.expanduser("~")) + '/'
 
-DEBUG = 1
+DEBUG = 0
 
 # Fragile lists:
 # ===========================================================================
