4f4df29999dac47373185f576ae88965a8156680
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-07 10:31:41 -0500

    automated push

--- test.py
+++ test.py
@@ -63,12 +63,32 @@ m.train()
 
 # -------------------------------------------------------------
 
+curs = [
+# Reproduce
+    'master.PbS QD.bare qd testing.Q0 Reproduce.Pb108S108.g opt',
+    'master.PbS QD.bare qd testing.Q0 Reproduce.Pb68S68.g opt',
+    'master.PbS QD.bare qd testing.Q0 Reproduce.Pb63S62.g opt',
+    'master.PbS QD.bare qd testing.Q0 Reproduce.Pb55S38.g opt',
+# Test convergence
+    'master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - regular grid | end - -0_02 | omg i forgot isym0',
+    'master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - regular grid | end - -0_02',
+    'master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - perturbed | end - -0_02',
+    'master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - perturbed #2 | end - -0_02',
+# Crunchit
+    'master.PbS QD.bare qd testing.crunchit.3 opt',
+    'master.PbS QD.bare qd testing.crunchit.4 opt',
+    'master.PbS QD.bare qd testing.crunchit.5 opt',
+    'master.PbS QD.bare qd testing.crunchit.6 opt',
+    'master.PbS QD.bare qd testing.crunchit.7 opt',
+]
 
 dynamic.global_load()
 m = dynamic.MlPbSOpt()
-for n in engine.Map().lookup('master').map.traverse():
-    if getattr(n, 'vasp', None) and getattr(n.vasp, 'optimized_cell', None):
-        m.parse_obj(n.vasp)
-    # except (shared.CustomError, shared.DeferError, AttributeError) as e:
-    #     print 'warning: node %s\'s parsing failed. probably old version.' %n.name
+# for n in engine.Map().lookup('master').map.traverse():
+#     if getattr(n, 'vasp', None) and getattr(n.vasp, 'optimized_cell', None):
+#         m.parse_obj(n.vasp)
+for cur in curs:
+    n = engine.Map().lookup(cur)
+    print 'parsing cur %s' %cur
+    m.parse_obj(n.vasp)
 m.train()
