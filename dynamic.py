ef720243948747aefa2037577e25a8dc2e279eec
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-06 21:00:59 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -183,6 +183,7 @@ class MlVaspSpeed(object):
             saver.restore(sess, self.path)
             for _ in range(n_epochs * _X.shape[0] / batch_size):
                 batch_idx = np.random.choice(_X.shape[0], size=batch_size)
+                IPython.embed()
                 sess.run([update_ops, training_op], feed_dict={_X_batch: _X[batch_idx], _y0_batch: _y0[batch_idx]})
             saver.save(sess, self.path)
 
