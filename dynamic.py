50888efec55912e319d418b1e74f4d6505228a5c
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-08 18:37:06 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -220,7 +220,7 @@ class MlVaspSpeed(object):
         # batch: random.choice
         # ann
         criterion = nn.MSELoss()
-        optimizer = optim.SGD(self.net.parameters(), lr=learning_rate)
+        optimizer = optim.Adam(self.net.parameters(), lr=learning_rate)
         # train
         self.net.train()
         for epoch in range(n_epochs):
