91e8c3c833b890a38a65882133c49337ce8d1c76
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-08 14:47:25 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -225,23 +225,23 @@ class MlVaspSpeed(object):
 
 
 # inital training script for MlVaspSpeed
-dynamic.global_load()
-m = dynamic.MlVaspSpeed()
-for n in engine.Map().lookup('master').map.traverse():
-    try:
-        n.cell = engine.Cell(str(n.cell))
-        n.gen.cell = n.cell
-        n.vasp.cell = n.cell
-        n.vasp.gen = n.gen
-        n.vasp.optimized_cell = engine.Cell(str(n.vasp.optimized_cell))
-    except AttributeError:
-        pass
-    if getattr(n, 'gen', None) and n.gen.parse_if('engine=vasp') and n.moonphase()==2:
-        try:
-            m.parse_obj(n.vasp, engine.Makeparam(n.vasp.gen))
-        except (shared.CustomError, shared.DeferError) as e:
-            print 'warning: node %s\'s parsing failed. probably old version.' %n.name
-m.train()
+# dynamic.global_load()
+# m = dynamic.MlVaspSpeed()
+# for n in engine.Map().lookup('master').map.traverse():
+#     try:
+#         n.cell = engine.Cell(str(n.cell))
+#         n.gen.cell = n.cell
+#         n.vasp.cell = n.cell
+#         n.vasp.gen = n.gen
+#         n.vasp.optimized_cell = engine.Cell(str(n.vasp.optimized_cell))
+#     except AttributeError:
+#         pass
+#     if getattr(n, 'gen', None) and n.gen.parse_if('engine=vasp') and n.moonphase()==2:
+#         try:
+#             m.parse_obj(n.vasp, engine.Makeparam(n.vasp.gen))
+#         except (shared.CustomError, shared.DeferError) as e:
+#             print 'warning: node %s\'s parsing failed. probably old version.' %n.name
+# m.train()
 
 
 
