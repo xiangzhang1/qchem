7dce25b45600b404440dc9ec62fcabb42119bceb
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-10 00:26:21 -0500

    automated push

--- MlPbSOpt.py
+++ MlPbSOpt.py
@@ -49,10 +49,10 @@ curs = [
 # #     # 'master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - perturbed | end - -0_02',
 # #     # 'master.PbS QD.bare qd testing.Q0 Test convergence.Pb55S38.start - perturbed #2 | end - -0_02',
 # # Crunchit
-    'master.PbS QD.bare qd testing.crunchit.3 opt',
-    'master.PbS QD.bare qd testing.crunchit.4 opt',
-    'master.PbS QD.bare qd testing.crunchit.5 opt',
-    'master.PbS QD.bare qd testing.crunchit.6 opt',
+    # 'master.PbS QD.bare qd testing.crunchit.3 opt',
+    # 'master.PbS QD.bare qd testing.crunchit.4 opt',
+    # 'master.PbS QD.bare qd testing.crunchit.5 opt',
+    # 'master.PbS QD.bare qd testing.crunchit.6 opt',
     'master.PbS QD.bare qd testing.crunchit.7 opt',
 ]
 
