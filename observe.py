b5a7e4d627d7697f1954c04ceb8940909960a627
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-10 15:46:04 -0500

    automated push

--- observe.py
+++ observe.py
@@ -1,12 +1,15 @@
+#data
 _X = np.c_[np.arange(3, 20, 0.1)]
 _y0 = np.c_[1.0 / _X ** 2]
 
-_X_scaler = StandardScaler()
-_y0_scaler = StandardScaler()
+# pipeline
+_X_pipeline = StandardScaler()
+_y0_pipeline = StandardScaler()
 
-_X_scaled = _X_scaler.fit_transform(_X)
-_y0_scaled = _y0_scaler.fit_transform(_y0)
+_X_scaled = _X_pipeline.fit_transform(_X)
+_y0_scaled = _y0_pipeline.fit_transform(_y0)
 
+# ann
 net = Sequential(
 nn.Linear(1,5),
 nn.ReLU(),
@@ -14,19 +17,31 @@ nn.Linear(5,5),
 nn.ReLU(),
 nn.Linear(5,1)
 )
-net.train()
 criterion = nn.MSELoss()
-optimizer = optim.SGD()
+optimizer = optim.Adam(net.parameters(), lr=0.01)
 
-ds = TensorDataset(_X_scaled, _y0_scaled)
-dl = DataLoader(ds, batch_size=32)
+# batch
+ds = TensorDataset(torch.FloatTensor(_X_scaled), torch.FloatTensor(_y0_scaled))
+dl = DataLoader(ds, batch_size=32, shuffle=True)
 
+# train
+net.train()
 n_epochs = 1000
-for epoch in range(n_epochs):
+for epoch in tqdm(range(n_epochs)):
     for _X_batch, _y0_batch in dl:
-        X, y0 = Variable(_X_batch, requires_grad=True), Variable(_y0_batch, requires_grad=True)
+        X, y0 = Variable(_X_batch, requires_grad=True), Variable(_y0_batch, requires_grad=False)
         y = net(X)
+        net.zero_grad()
         loss = criterion(y, y0)
         loss.backward()
-        net.zero_grad()
         optimizer.step()
+
+# test
+net.eval()
+X = Variable(torch.FloatTensor(_X_scaled), requires_grad=True)
+y = net(X)
+_y_scaled = y.data.numpy()
+_y = _y0_pipeline.inverse_transform(_y_scaled)
+
+plt.plot(_X, _y, '-', _X, _y0, '--')
+plt.show()
