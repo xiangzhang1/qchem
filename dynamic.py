85e662d1b6cdb3a6d88b0776d9b5a922d0fbfa79
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-11 12:39:52 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -325,15 +325,14 @@ class MlPbSOpt(object):
         # ann
         dropout_p = 0.05
         self.net = Sequential(
-            nn.Linear(5,10),
+            nn.Linear(5,40),
             nn.ELU(),
-            nn.Dropout(p=dropout_p),
-            nn.Linear(10,20),
+            nn.Linear(40,30),
             nn.ELU(),
-            nn.Dropout(p=dropout_p),
+            nn.Linear(30,20),
+            nn.ELU(),
             nn.Linear(20,10),
             nn.ELU(),
-            nn.Dropout(p=dropout_p),
             nn.Linear(10,3)
         )
 
@@ -368,7 +367,7 @@ class MlPbSOpt(object):
             self._y0.append(label)
 
 
-    def train(self, n_epochs=200, learning_rate=0.0001, optimizer_name='Adam'):
+    def train(self, n_epochs=100, learning_rate=0.001, optimizer_name='Adam'):
 
         # train
         # pipeline
