85c774d88e7f6eaf08c177a2d64b4a695f5bbaab
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-11 18:59:31 -0500

    automated push

--- MlPbSOpt.py
+++ MlPbSOpt.py
@@ -67,9 +67,6 @@ for cur in curs:
 m2 = dynamic.MlPbSOptL2()
 m2.parse_train(*m1.train(n_epochs=1))
 
-m1.train()
-
-
 
 
 
--- dynamic.py
+++ dynamic.py
@@ -490,11 +490,11 @@ class MlPbSOptL2(object):
         self._X = []
         self._y0 = []
         # scaler
-        self._X_pipeline = StandardScaler()
-        self._y_pipeline = StandardScaler()
+        self.X_pipeline = StandardScaler()
+        self.y_pipeline = StandardScaler()
         # ANN(deep)
-        self.ann = Sequential(
-            nn.Conv3d(1, 4, kernel_size=2),
+        self.net = Sequential(
+            nn.Conv3d(2, 4, kernel_size=2),
             nn.ReLU(),
             nn.MaxPool3d(2),
             nn.Conv3d(4, 8, kernel_size=2),
@@ -515,7 +515,9 @@ class MlPbSOptL2(object):
 
         # train
         # pipeline
-        _X = self.X_pipeline.fit_transform(self._X)[train_idx]
+        _X = np.array(self._X)
+        shape = _X.shape
+        _X = self.X_pipeline.fit_transform(_X.reshape(shape[0], -1)).reshape(_X.shape)[train_idx]
         _y0 = self.y_pipeline.fit_transform(self._y0)[train_idx]
         # batch: random.choice
         # ann
@@ -548,19 +550,22 @@ class MlPbSOptL2(object):
         for i in range(len(X1)):
             X1_, y1_ = X1[i], y1[i]
             #
-            X2_ = np.zeros((11,11,11))
+            X2_ = np.zeros((2,11,11,11))    # 2 channels
             for c in X1_:
-                rc = np.around(c[:3] / a) * a + 5
+                rc = np.int32(np.around(c[:3] / a)) + 5
                 if all(-1<x<11 for x in rc):
-                    X2_[rc] = c[3] * c[4]
-            self._X.append(X2_)
+                    X2_[0][rc] = c[3] * c[4]
+                    X2_[1][rc] = 1
+            self._X.append(_X2)
             # y2_ = y1_
             self._y0.append(y1_)
 
 
     def predict(self, _X):
         # pipeline
-        _X = self.X_pipeline.transform(_X)
+        _X = np.array(_X)
+        shape = _X.shape
+        _X = self.X_pipeline.fit_transform(_X.reshape(shape[0], -1)).reshape(_X.shape)[train_idx]
         # ann
         self.net.eval()
         y = self.net(Variable(torch.FloatTensor(_X), requires_grad=True))
