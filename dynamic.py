f9bc632531f09d965241847cf4c9f4ce206814c9
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-11 19:14:01 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -555,8 +555,8 @@ class MlPbSOptL2(object):
             for c in X1_:
                 rc = np.int32(np.around(c[:3] / a)) + 5
                 if all(-1<x<11 for x in rc):
-                    X2_[0][rc] = c[3] * c[4]
-                    X2_[1][rc] = 1
+                    X2_[0,rc[0],rc[1],rc[2]] = c[3] * c[4]
+                    X2_[0,rc[0],rc[1],rc[2]] = 1
             self._X.append(X2_)
             # y2_ = y1_
             self._y0.append(y1_)
