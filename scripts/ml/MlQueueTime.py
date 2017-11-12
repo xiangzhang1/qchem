eb2e8bc1865e7faf514a9f28d24e786affecf47a
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-12 16:32:38 -0500

    automated push

--- scripts/ml/MlQueueTime.py
+++ scripts/ml/MlQueueTime.py
@@ -5,5 +5,5 @@ for n in engine.Map().lookup('master').map.traverse():
         print 'parsing %s' %n.name
         try:
             m.parse_train(n, n.gen, n.vasp)
-        except (shared.CustomError, shared.DeferError) as e:
+        except (shared.CustomError, shared.DeferError, StopIteration) as e:
             print 'warning: node %s\'s parsing failed. probably old version.' %n.name ; sys.stdout.flush()
