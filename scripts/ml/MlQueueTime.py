ecc3c5fbda2050512bd8f15ad2deb09087fd2648
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-12 16:18:48 -0500

    automated push

--- scripts/ml/MlQueueTime.py
+++ scripts/ml/MlQueueTime.py
@@ -2,7 +2,4 @@ dynamic.global_load()
 m = dynamic.MlQueueTime()
 for n in engine.Map().lookup('master').map.traverse():
     if getattr(n, 'vasp', None):
-        try:
-            m.parse_train(n, n.vasp, n.gen, n.cell, engine.Makeparam(n.gen))
-        except (shared.CustomError, shared.DeferError) as e:
-            print 'warning: node %s\'s parsing failed. probably old version.' %n.name ; sys.stdout.flush()
+        m.parse_train(n, n.vasp, n.gen, n.cell, engine.Makeparam(n.gen))
