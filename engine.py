b34fec06151623ad4885565f7ccd9a4546f0f039
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-12 16:22:42 -0500

    automated push

--- engine.py
+++ engine.py
@@ -983,7 +983,6 @@ class Vasp(object):
             elif platform in ['nanaimo', 'irmik', 'comet', 'edison', 'cori']:
                 output = self.ssh_and_run("sacct -S 0101 --format=jobname%%100,submit,start -u xzhang1 --name=%s" %(self.remote_folder_name)).splitlines()
                 lines = [l.split() for l in output]
-                IPython.embed()
                 submit = next(l for l in lines if l[0]==self.remote_folder_name)[1]
                 submit = time.mktime(dateparser.parse(submit).timetuple())
                 start = next(l for l in lines if l[0]==self.remote_folder_name)[2]
