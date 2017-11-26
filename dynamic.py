fd13c25514a16bda5adfe408d7505df6bf922694
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-26 18:12:35 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -636,9 +636,9 @@ class MlPbSOptXCE(object):
             optimizer.step()
 
             if np.random.rand() < 1.0 / 200:
-                invy = self.y_pipeline.inverse_transform(_f.reshape(1,-1))[0]
-                invy0 = self.y_pipeline.inverse_transform(_f0.reshape(1,-1))[0]
-                pred = self.predict(_X1[i])
+                invy = np.array(self.y_pipeline.inverse_transform(_f.reshape(1,-1))[0])
+                invy0 = np.array(self.y_pipeline.inverse_transform(_f0.reshape(1,-1))[0])
+                pred = np.array(self.predict(_X1[i]))
                 _loss = np.asscalar(loss.data.cpu().numpy())
                 indices.set_description('y %s, y0 %s, invy %s, invy0 %s, pred %s, loss %s' %(_f, _f0, invy, invy0, pred, _loss))
 
