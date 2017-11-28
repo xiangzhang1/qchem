5405cd5bfef9a7ece1bf75d46e05c700eadf5771
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-28 03:54:46 -0500

    automated push

--- snippets/ml_related/get_started_lstm.py
+++ snippets/ml_related/get_started_lstm.py
@@ -1,31 +1,58 @@
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM, Dense, Masking
+from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
-data_dim = 16
-timesteps = 8
-num_classes = 10
+nsamples = 3000
+timesteps = 600
+data_dim = 4
 
 # expected input data shape: (batch_size, timesteps, data_dim)
 model = Sequential()
-model.add(LSTM(32, return_sequences=True,
-               input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
-model.add(LSTM(32))  # return a single vector of dimension 32
-model.add(Dense(10, activation='softmax'))
+model.add(Masking(mask_value=0.0,
+               input_shape=(timesteps, data_dim)))
+model.add(LSTM(8, return_sequences=True))  # returns a sequence of vectors of dimension 32
+model.add(LSTM(8))  # return a single vector of dimension 32
+model.add(Dense(8, activation='relu'))
+model.add(Dense(24, activation='relu'))
+model.add(Dense(1))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+model.compile(loss='mean_absolute_percentage_error',
+              optimizer='adam',
               metrics=['accuracy'])
 
 # Generate dummy training data
-x_train = np.random.random((1000, timesteps, data_dim))
-y_train = np.random.random((1000, num_classes))
+x_train = []
+y_train = []
+for i in range(1000):
+    timesteps_ = np.random.randint(low=0, high=timesteps)
+    x_train_ = np.random.random((timesteps_, data_dim)) - 0.5
+    y_train_ = np.sum(np.linalg.norm(x_train_, axis=0), keepdims=True)
+    x_train.append(x_train_)
+    y_train.append(y_train_)
+
+x_train = pad_sequences(x_train, dtype='float32', maxlen=timesteps)
+y_train = np.array(y_train)
 
 # Generate dummy validation data
-x_val = np.random.random((100, timesteps, data_dim))
-y_val = np.random.random((100, num_classes))
+x_val = []
+y_val = []
+for i in range(500):
+    timesteps_ = np.random.randint(low=0, high=timesteps)
+    x_val_ = np.random.random((timesteps_, data_dim)) - 0.5
+    y_val_ = np.sum(np.linalg.norm(x_val_, axis=0), keepdims=True)
+    x_val.append(x_val_)
+    y_val.append(y_val_)
+
+x_val = pad_sequences(x_val, dtype='float32', maxlen=timesteps)
+y_val = np.array(y_val)
+
+# Genreate dummy training data
 
 model.fit(x_train, y_train,
-          batch_size=64, epochs=5,
+          batch_size=64, epochs=80,
           validation_data=(x_val, y_val))
+
+y_predict = model.predict(x_val)
+model.evaluate(x_val, y_val)
+plt.scatter(y_val, y_predict)
