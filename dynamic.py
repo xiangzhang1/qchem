36daa0c73e95798d4f970ae1ece2fe5bf7f8b3f9
Xiang Zhang <zhang-xiang@hotmail.com>
2017-11-30 03:44:59 -0500

    automated push

--- dynamic.py
+++ dynamic.py
@@ -534,7 +534,7 @@ class MlPbSOptXRNN(object):
             self._X += list(X)
             self._y0 += list(y0)
 
-    def train(self, batch_size=12, epochs=500):
+    def train(self, batch_size=12, epochs=300):
         # pipeline
         model = self.model
         data_dim = self.data_dim
